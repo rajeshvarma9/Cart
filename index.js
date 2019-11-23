608efbae7cdaf6bfeb7fa19f5cf6982a00624922
Rajesh Varma <rajeshvarma4195@gmail.com>
2019-11-23 21:21:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function addbutton() {
+    var x = event.target.id;
+	var element = '<h6 onclick="deletebutton()" id=' + event.target.id + '>' + document.getElementById(event.target.id).innerHTML + '</h6>';
+	document.getElementById('add').style.opacity = 1;
+	document.getElementById('add').onclick = function() {
+	  document.getElementById('add').style.opacity = 0.5;
+	  $('#' + x).remove();
+      $('#cart').append(element);
+      $('#msg1').remove();
+      var y = document.getElementById('options').innerHTML.trim();
+      if(y=="") {
+	      $('#options').append('<h6 id="msg2">It seems you are very hungry!</h6>');
+        }
+ 	 };	
+}
+
+function deletebutton() {
+    var x = event.target.id;
+	var element = '<h6 onclick="addbutton()" id=' + event.target.id + '>' + document.getElementById(event.target.id).innerHTML + '</h6>';
+	document.getElementById('delete').style.opacity = 1;
+	document.getElementById('delete').onclick = function() {
+	  document.getElementById('delete').style.opacity = 0.5;
+	  $('#' + x).remove();
+      $('#options').append(element);
+      $('#msg2').remove();
+      var z = document.getElementById('cart').innerHTML.trim();
+      if(z=="") {
+	      $('#cart').append('<h6 id="msg1">Your cart is empty!</h6>');
+        }
+ 	};	
+}
+
+var x = document.getElementById('cart').innerHTML.trim();
+if(x=="") {
+	$('#cart').append('<h6 id="msg1">Your cart is empty!</h6>');
+}
